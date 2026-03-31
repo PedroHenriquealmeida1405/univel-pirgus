9c7f5628261965f4f54bc82a1ab4e23bf860edb4
PedroHenriquealmeida1405 <suckeldre@gmail.com>
2026-03-30 22:02:45 -0300

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,53 +4,52 @@ void main() {
   runApp(MeuApp());
 }
 
-class MeuApp extends StatelessWidget {
-  MeuApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  class MeuApp extends StatelessWidget {
+    MeuApp{(super.key)};
+    
+    @override
+    Widget build(BuildContext context) {
+      return
+      MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.purpleAccent,
           title: Center(
-            child: Text("nome do meu app"),
-          ),
+            child: Text("nome do meu app")
+            ),
         ),
-        body: PaginaInicial(),
+        body: PaginaInicial{}
       ),
-    );
+    ),
+  
+    }
   }
-}
-
-class PaginaInicial extends StatefulWidget {
-  @override
-  State<PaginaInicial> createState() => PaginaInicialState();
-}
-
-class PaginaInicialState extends State<PaginaInicial> {
-  int contador = 0;
-  String texto = 'Olá, mundo!';
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(texto),
-          ElevatedButton(
-            child: Text('Mudar texto'),
-            onPressed: () {
-              setState(() {
-                contador++;
-                texto = "o texto foi alterado $(++contador) $(contador > 1 ? "vezes" : "vez";
-              });
-            },
-          ),
-        ],
-      ),
-    );
+  class PaginaInicial extends StatefulWidget{
+    @override
+    State<PaginaInicial> createState() => PaginaInicialState();
   }
-}
 
+  class PaginaInicialState extends State<PaginaInicial>{
+    String texto = "Olá, mundo";
+
+    @override
+    Widget build(buildContext context) {
+      return Center(
+        child: Column(
+          mainAxisAligment: MainAxisAligment.center,
+          children: {
+            Text(texto),
+            ElevatedButton(
+              child: Text('mudar texto'),
+              onPressed: () {
+                setState(() {
+                  contator++;
+                  texto = "o texto foi alterado ${++contador} ${contador > 1 ? "vezes" : "vez"}"; 
+                });
+              },
+            )
+          },
+        ),
+      )
+    }
+  }

effca7a9857a90fbb42aa88a0e59a1aa6c05bbf5
PedroHenriquealmeida1405 <suckeldre@gmail.com>
2026-03-30 21:08:12 -0300

    main.dart
    Fix syntax errors and improve widget structure.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+
+  );
+}
+
+class Meupp extends Statelesswidget {
+  MeuApp{(super.key)}
+  @override
+  Widget build(BuildContext BuildContext) {
+    return
+
+        MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.purpleAccent,
@@ -21,5 +31,30 @@ void main() {
         ),
       ),
     ),
-  );
+  }
+}
+class _PaginaInicial extends StatefulWidget {
+  @override
+  State<PaginaInicial> createState() => PaginaInicialState();
+}
+
+Class PaginaInicial extends State<PaginaInicial> {
+  String texto = "olá, mundo!"
+  @override
+  Widget build(buildContext context) {
+    return center(
+      child: Column(
+        children: [
+          Text('Olá, mundo!')
+          ElevateButton(
+            child: Text('mudar texto'),
+            onPressed:() {
+              texto = "texto alterado";
+
+            },
+          )
+        ],
+      )
+    )
+  }
 }
